8a192b3e522ded72f4aceca5cdb14e4c53e484a3
xizhaobo <710285592@qq.com>
2020-09-27 16:16:10 +0800

    state下initState及initData学习

---
+++ vue-src/src/core/instance/index.js
@@ -0,0 +1,21 @@
+import { initMixin } from './init'
+import { stateMixin } from './state' // 初始化data，props，methods，computed，watch
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+function Vue(options) {
+  if (process.env.NODE_ENV !== 'production' && !(this instanceof Vue)) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+renderMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+
+export default Vue
